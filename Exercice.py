d09465bc0c9ef4282487bc574058109d8114eade
Sbelgana <44445774+Sbelgana@users.noreply.github.com>
2021-03-09 21:37:41 -0500

    Add files via upload

---
+++ Exercice.py
@@ -0,0 +1,38 @@
+def read_file(...):
+    #TO-DO: Ouverture du fichier en mode lecture
+
+    
+    #TO-DO: lire le contenue du fichier
+	 
+    return ...
+
+def word_count(...):
+    # TO-DO: Faites votre nettoyage du texte (preprocessing)
+
+  
+    # TO-DO: Calculer l'occurrence des mots et ajoute dans un dictionnaire, trouver le mot le plus long/court 
+    # calculer aussi la longueur moyenne des mots du texte.
+    
+
+    return ...
+
+
+
+def write_file(...):
+    
+    #TO-DO: Utiliser les fonctions read_file et word_count
+	
+    
+    #TO-DO: Ouverture du fichier en mode écriture
+
+    
+    #TO-DO: Ecrire les données dans le fichier
+	
+    
+    #TO-DO: Fermeture du fichier
+    f.close()
+
+if __name__ == "__main__":
+	path_write = "database/Result.txt"
+	path_read = "database/text.txt"
+    write_file(...)
